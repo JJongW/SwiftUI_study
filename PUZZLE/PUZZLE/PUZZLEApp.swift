13692f96d72c89b0236cf1377eed5d1bb24be3e0
JJongW <shinjw4675@gmail.com>
2023-03-15 17:05:53 +0900

    [Chapter1] Viewtest

---
+++ PUZZLE/PUZZLE/PUZZLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  PUZZLEApp.swift
+//  PUZZLE
+//
+//  Created by JW on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct PUZZLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
